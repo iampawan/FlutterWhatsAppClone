e2d960e0937a2fe743b00f2b7e945dae9ffb95bf
sanni prasad <prasadsunny1@gmail.com>
2020-08-22 00:51:55 +0530

    another file

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\SDKS\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\PROJECTS\atemp\flutterwhatsapp"
+export "FLUTTER_APPLICATION_PATH=C:\PROJECTS\FlutterWhatsAppClone"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
