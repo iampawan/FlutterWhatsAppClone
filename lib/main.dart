62cef2cec92886c731a77af13345535f902de0d6
dgbkn <65810521+dgbkn@users.noreply.github.com>
2020-10-06 11:32:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "WhatsApp",
+      title: "WhatsApp Clone Using Flutter",
       theme: new ThemeData(
-        primaryColor: new Color(0xff075E54),
-        accentColor: new Color(0xff25D366),
+        primaryColor: new Color(0xff078E54),
+        accentColor: new Color(0xff25D966),
       ),
       debugShowCheckedModeBanner: false,
       home: new WhatsAppHome(cameras:cameras),
